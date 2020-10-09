2110fad1be75339731a6c570f256de83e714778c
sumamss153 <65152304+sumamss153@users.noreply.github.com>
2020-10-09 17:30:12 +0530

    Update resp_req_Jira.py

--- Test_SP/resp_req_Jira.py
+++ Test_SP/resp_req_Jira.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.get('https://myproject.atlassian.net/rest/api/2/search?jql=project="myproject"&maxResults=20', auth=('learn.atsj@gmail.com', 'B7SaYPPeHQqR1iQXnYC09729'))
+r = requests.get('https://learnatsj.atlassian.net/rest/api/2/search?jql=project="myproject"&maxResults=20', auth=('learn.atsj@gmail.com', 'B7SaYPPeHQqR1iQXnYC09729'))
 
 
 for k in response.json().keys():
